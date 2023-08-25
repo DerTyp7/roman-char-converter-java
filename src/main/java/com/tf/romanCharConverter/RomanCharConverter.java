c5f1c153940edc2bf4e47525be619ed67cd3f790
DerTyp7 <janis@tealfire.de>
2023-08-25 11:48:11 +0200

    init

---
+++ src/main/java/com/tf/romanCharConverter/RomanCharConverter.java
@@ -0,0 +1,37 @@
+package com.tf.romanCharConverter;
+
+import java.util.HashMap;
+
+public class RomanCharConverter {
+
+    private final HashMap<String, Integer> romanToArabic = new HashMap<String, Integer>();
+    public RomanCharConverter() {
+        this.romanToArabic.put("I", 1);
+        this.romanToArabic.put("V", 5);
+        this.romanToArabic.put("X", 10);
+        this.romanToArabic.put("L", 50);
+        this.romanToArabic.put("C", 100);
+        this.romanToArabic.put("D", 500);
+        this.romanToArabic.put("M", 1000);
+        this.romanToArabic.put("A", 5000);
+    }
+
+    public int convert(String romanString) {
+        int result = 0;
+        for (int i = 0; i < romanString.length(); i++) {
+            String romanChar = String.valueOf(romanString.charAt(i));
+
+            if(i < romanString.length() - 1) {
+                String nextRomanChar = String.valueOf(romanString.charAt(i + 1));
+                if(this.romanToArabic.get(romanChar) < this.romanToArabic.get(nextRomanChar)) {
+                    result -= this.romanToArabic.get(romanChar);
+                    continue;
+                }
+            }
+
+            result += this.romanToArabic.get(romanChar);
+
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/tf/romanCharConverter/RomanCharConverterTest.java
@@ -0,0 +1,51 @@
+package com.tf.romanCharConverter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanCharConverterTest {
+    private RomanCharConverter converter;
+
+    @BeforeEach
+    public void setUp() {
+        converter = new RomanCharConverter();
+    }
+
+    @Test
+    public void givenSingleRomanChar_whenConvert_thenArabic(){
+        assertEquals(10, converter.convert("X"));
+        assertEquals(50, converter.convert("L"));
+        assertEquals(100, converter.convert("C"));
+        assertEquals(500, converter.convert("D"));
+        assertEquals(1000, converter.convert("M"));
+        assertEquals(5000, converter.convert("A"));
+    }
+
+    @Test
+    public void givenMultipleRomanChar_whenConvert_thenArabic(){
+        assertEquals(2, converter.convert("II"));
+        assertEquals(20, converter.convert("XX"));
+        assertEquals(200, converter.convert("CC"));
+        assertEquals(2000, converter.convert("MM"));
+        assertEquals(10000, converter.convert("AA"));
+
+        assertEquals(3, converter.convert("III"));
+        assertEquals(30, converter.convert("XXX"));
+        assertEquals(300, converter.convert("CCC"));
+        assertEquals(3000, converter.convert("MMM"));
+        assertEquals(1500, converter.convert("MD"));
+    }
+
+    @Test
+    public void givenMultipleRomanSubstractChar_whenConvert_thenArabic(){
+        assertEquals(4, converter.convert("IV"));
+        assertEquals(9, converter.convert("IX"));
+        assertEquals(40, converter.convert("XL"));
+        assertEquals(90, converter.convert("XC"));
+        assertEquals(400, converter.convert("CD"));
+        assertEquals(900, converter.convert("CM"));
+        assertEquals(14, converter.convert("XIV"));
+    }
+}
